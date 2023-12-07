0d457cd2f482c9b2760118e37aae8b452bae829b
anikprofakeacc <149324783+anikprofakeacc@users.noreply.github.com>
2023-12-07 22:22:27 +0530

    Update chatgpt.py

--- DAXXMUSIC/plugins/tools/chatgpt.py
+++ DAXXMUSIC/plugins/tools/chatgpt.py
@@ -63,3 +63,46 @@ async def chat(app :app, message):
 
 
 ##### Bing
+@app.on_message(
+    filters.command(
+        ["bard", "askbard"], prefixes=["+", ".", "/", "-", "?", "$", "#", "&"]
+    )
+)
+
+async def bardapibot(bot, message):
+    try:
+        if message.reply_to_message:
+            if message.reply_to_message.text:
+                text = message.reply_to_message.text
+            
+                response = requests.get(url).json()
+                text= response["text"]
+
+            else:
+                return await message.reply_text("ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴍᴇssᴀɢᴇ ᴏʀ ɢɪᴠᴇ sᴏᴍᴇ ᴛᴇxᴛ.")
+        else:
+            try:
+                text = message.text.split(None, 1)[1]
+
+            except IndexError:
+                return await message.reply_text(
+                    "ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴍᴇssᴀɢᴇ ᴏʀ ɢɪᴠᴇ sᴏᴍᴇ ᴛᴇxᴛ.\n Exᴀᴍᴘʟᴇ:\n\n/bard How r u?"
+                )
+        m = await message.reply_text(
+                f"{message.from_user.first_name} Pʀᴏᴄᴇssɪɴɢ ʏᴏᴜʀ Qᴜᴇʀʏ....."
+            )
+        url = f"https://api.safone.dev/bard?message={text}"
+        response = requests.get(url)
+        out = response.json()
+        output = out["choices"][0]["content"][0]
+        
+        await bot.send_chat_action(message.chat.id, ChatAction.TYPING)
+        
+        await m.reply(f"{output}\n\n🎉ᴘᴏᴡᴇʀᴇᴅ ʙʏ @catchme_here",
+                                parse_mode=ParseMode.MARKDOWN,
+                                
+                            )
+                
+
+    except Exception as e:
+        await message.reply_text(f"ᴇʀʀᴏʀ: {e}")
